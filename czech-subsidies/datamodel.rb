0797461f47b83d79a4f52154d8f1bfc2fbc82e9c
carladrago <carladrago@gmail.com>
2013-08-23 13:30:39 +0200

    czech folder added

---
+++ czech-subsidies/datamodel.rb
@@ -0,0 +1,47 @@
+require 'rubygems'
+require 'data_mapper'
+
+DataMapper.setup(:default, "postgres://localhost/farmsubsidy_development")
+
+
+class Recipient
+	include DataMapper::Resource
+	property :id, Serial
+	property :global_recipient_id, String
+	property :zipcode, String
+	property :admin_area_1, String
+	property :admin_area_2, String
+end
+
+class Payment
+	include DataMapper::Resource
+	property :id, Serial
+	property :recipientId, String
+	property :year, Integer
+	property :amount, Float
+end
+
+class AdminArea1
+	property :id, Serial
+	property :name, String
+	property :country_id, Integer
+end
+
+class AdminArea2
+	property :id, Serial
+	property :name, String
+	property :admin_area_1_id, Integer
+end
+
+class Country
+	property :id, Serial
+	property :name, String
+	property :code, Integer
+end
+
+
+
+DataMapper.finalize
+
+
+
